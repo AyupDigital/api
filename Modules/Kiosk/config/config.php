9f5449a85c964f1c51a3a09d471070762758b845
byt3sage <jtooleuk@gmail.com>
2025-02-13 09:33:09 +0000

    chore: changed default app urlg

--- Modules/Kiosk/config/config.php
+++ Modules/Kiosk/config/config.php
@@ -4,7 +4,7 @@ return [
     'name' => 'Kiosk',
     'deploymentName' => 'Hounslow Connect Kiosk',
     'frontendUrl' => 'https://hounslowconnect.com',
-    'apiBaseUrl' => env('APP_URL', 'https://api.staging.hounslowconnect.com'),
+    'apiBaseUrl' => env('APP_URL', 'https://api.hounslowconnect.com'),
     'slack' => [
         'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
         'channel' => env('SLACK_CHANNEL')
