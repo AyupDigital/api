dd847778e2c3877fa086f20ea6a1e2a7873fdd97
byt3sage <jtooleuk@gmail.com>
2025-02-19 11:22:13 +0000

    chore: updates

--- app/Http/Controllers/Core/V1/SearchController.php
+++ app/Http/Controllers/Core/V1/SearchController.php
@@ -12,81 +12,81 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class SearchController extends Controller
 {
-    public function __invoke(
-        Request $request,
-        SearchCriteriaQuery $criteria,
-        ServiceQueryBuilder $builder,
-        ServiceEloquentMapper $mapper
-    ): AnonymousResourceCollection {
-        // Apply query.
-        if ($request->has('query')) {
-            $criteria->setQuery($request->input('query'));
-        }
-
-        if ($request->has('category')) {
-            // If category given then filter by category.
-            $criteria->setCategories(explode(',', $request->input('category')));
-        }
-
-        if ($request->has('persona')) {
-            // Otherwise, if persona given then filter by persona.
-            $criteria->setPersonas(explode(',', $request->input('persona')));
-        }
-
-        // Apply filter on `wait_time` field.
-        if ($request->has('wait_time')) {
-            $criteria->setWaitTime($request->input('wait_time'));
-        }
-
-        // Apply filter on `is_free` field.
-        if ($request->has('is_free')) {
-            $criteria->setIsFree($request->input('is_free'));
-        }
-
-        // If polygon was passed, then parse the polygon.
-        if ($request->has('polygon')) {
-            $criteria->setPolygon($request->input('polygon'));
-        }
-
-        // If location was passed, then parse the location.
-        if ($request->has('location')) {
-            $criteria->setLocation(
-                new Coordinate(
-                    $request->input('location.lat'),
-                    $request->input('location.lon')
-                )
-            );
-
-            if ($request->has('distance')) {
-                $criteria->setDistance($request->input('distance'));
-            }
-        }
-
-        if ($request->has('eligibilities')) {
-            $criteria->setEligibilities($request->input('eligibilities'));
-        }
-
-        // Apply order.
-        if ($request->has('order')) {
-            $criteria->setOrder($request->input('order'));
-        }
-
-        // Get the pagination values
-        $page = page($request->input('page'));
-        $perPage = per_page($request->input('per_page'));
-
-        // Create the query
-        $esQuery = $builder->build(
-            $criteria,
-            $page,
-            $perPage
-        );
-
-        // Perform the search.
-        return $mapper->paginate(
-            $esQuery,
-            $page,
-            $perPage
-        );
+  public function __invoke(
+    Request $request,
+    SearchCriteriaQuery $criteria,
+    ServiceQueryBuilder $builder,
+    ServiceEloquentMapper $mapper
+  ): AnonymousResourceCollection {
+    // Apply query.
+    if ($request->has('query')) {
+      $criteria->setQuery($request->input('query'));
     }
+
+    if ($request->has('category')) {
+      // If category given then filter by category.
+      $criteria->setCategories(explode(',', $request->input('category')));
+    }
+
+    if ($request->has('persona')) {
+      // Otherwise, if persona given then filter by persona.
+      $criteria->setPersonas(explode(',', $request->input('persona')));
+    }
+
+    // Apply filter on `wait_time` field.
+    if ($request->has('wait_time')) {
+      $criteria->setWaitTime($request->input('wait_time'));
+    }
+
+    // Apply filter on `is_free` field.
+    if ($request->has('is_free')) {
+      $criteria->setIsFree($request->input('is_free'));
+    }
+
+    // If polygon was passed, then parse the polygon.
+    if ($request->has('polygon')) {
+      $criteria->setPolygon($request->input('polygon'));
+    }
+
+    // If location was passed, then parse the location.
+    if ($request->has('location')) {
+      $criteria->setLocation(
+        new Coordinate(
+          $request->input('location.lat'),
+          $request->input('location.lon')
+        )
+      );
+
+      if ($request->has('distance')) {
+        $criteria->setDistance($request->input('distance'));
+      }
+    }
+
+    if ($request->has('eligibilities')) {
+      $criteria->setEligibilities($request->input('eligibilities'));
+    }
+
+    // Apply order.
+    if ($request->has('order')) {
+      $criteria->setOrder($request->input('order'));
+    }
+
+    // Get the pagination values
+    $page = page($request->input('page'));
+    $perPage = per_page($request->input('per_page'));
+
+    // Create the query
+    $esQuery = $builder->build(
+      $criteria,
+      $page,
+      $perPage
+    );
+
+    // Perform the search.
+    return $mapper->paginate(
+      $esQuery,
+      $page,
+      $perPage
+    );
+  }
 }
