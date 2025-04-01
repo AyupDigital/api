5af02ab0f3bb0dcbfd73088d0fc0ae641b6f39c2
byt3sage <jtooleuk@gmail.com>
2025-04-01 14:09:27 +0100

    ACD-413

--- app/Rules/DateSanity.php
+++ app/Rules/DateSanity.php
@@ -3,7 +3,9 @@
 namespace App\Rules;
 
 use Carbon\Carbon;
+use ErrorException;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Log;
 
 class DateSanity implements ValidationRule
 {
@@ -22,16 +24,20 @@ class DateSanity implements ValidationRule
      */
     public function __construct(\Illuminate\Foundation\Http\FormRequest $request)
     {
-        if (isset($request->organisation_event)) {
-            $this->start = Carbon::parse($request->get('start_date', $request->organisation_event->start_date));
-            $this->end = Carbon::parse($request->get('end_date', $request->organisation_event->end_date));
-            $this->start->setTimeFromTimeString($request->get('start_time', $request->organisation_event->start_time));
-            $this->end->setTimeFromTimeString($request->get('end_time', $request->organisation_event->end_time));
-        } else {
-            $this->start = Carbon::parse($request->get('start_date', 'now'));
-            $this->end = Carbon::parse($request->get('end_date', 'now'));
-            $this->start->setTimeFromTimeString($request->get('start_time', '00:00:00'));
-            $this->end->setTimeFromTimeString($request->get('end_time', '00:00:00'));
+        try {
+            if (isset($request->organisation_event)) {
+                $this->start = Carbon::parse($request->get('start_date', $request->organisation_event->start_date));
+                $this->end = Carbon::parse($request->get('end_date', $request->organisation_event->end_date));
+                $this->start->setTimeFromTimeString($request->get('start_time', $request->organisation_event->start_time));
+                $this->end->setTimeFromTimeString($request->get('end_time', $request->organisation_event->end_time));
+            } else {
+                $this->start = Carbon::parse($request->get('start_date', 'now'));
+                $this->end = Carbon::parse($request->get('end_date', 'now'));
+                $this->start->setTimeFromTimeString($request->get('start_time', '00:00:00'));
+                $this->end->setTimeFromTimeString($request->get('end_time', '00:00:00'));
+            }
+        } catch (ErrorException $e) {
+            Log::info($e);
         }
     }
 
