088e0f45fd552be332829f8eabad69a69c31e18c
byt3sage <jtooleuk@gmail.com>
2025-06-20 11:02:40 +0100

    fix: issue with migration dropping non existent index

--- database/migrations/2025_03_13_134512_update_on_delete_on_service_tag_table.php
+++ database/migrations/2025_03_13_134512_update_on_delete_on_service_tag_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,8 +13,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('service_tag', function (Blueprint $table) {
-            $table->dropForeign(['service_id']);
-            $table->dropForeign(['tag_id']);
+            DB::statement('ALTER TABLE service_tag DROP FOREIGN KEY IF EXISTS service_tag_service_id_foreign');
+            DB::statement('ALTER TABLE service_tag DROP FOREIGN KEY IF EXISTS service_tag_tag_id_foreign');
 
             $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
